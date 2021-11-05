08c76d3da46506f28223b2a138ebf9ddfc904335
Simon Wood <simon.wood@holidayextras.com>
2021-11-05 19:48:02 +0000

    Working prototype

--- sortAlphabetical.py
+++ sortAlphabetical.py
@@ -1,7 +1,22 @@
-from os import walk
-
-filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
+from os import stat, utime, walk
+from datetime import datetime, timedelta
 
 # printing the list using loop
-for currentfile in range(len(filenames)):
-    print filenames[currentfile],
+def set_file_last_modified(filepath, dt):
+    dtepoch = dt.timestamp()
+    utime(filepath, (dtepoch, dtepoch))
+
+# get files from our folder
+mypath = './junk'
+files = next(walk(mypath), (None, None, []))[2]  # [] if no file
+print('Sorting folder '+mypath)
+
+# sort our files alphabetically
+files.sort()
+# subtract the number of files in seconds
+settime = datetime.now() - timedelta(seconds=len(files))
+
+# printing the list using loop
+for currentfile in range(len(files)):
+    set_file_last_modified(mypath+'/'+files[currentfile], settime + timedelta(seconds=1))
+    print(files[currentfile])
